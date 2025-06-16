f003d8bb80c0399e1128cd831356f5e3299213df
Sandesh <sandeshdahal063@gmail.com>
2025-06-16 08:18:22 +0545

    Added a proper login and signup system

---
+++ login_sys.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<string.h>
+
+void user_conf_page();
+void login_page();
+void signup_page();
+
+void user_conf_page(){
+    printf("----The Game Library----\n\n");
+    char options[][30] = {"Log In", "Sign Up"};
+    int ch = 0;
+    ch = create_menu_traditional(2, options, 1, "Choose an option: ");
+    
+    system("cls");
+    if (ch == 0) login_page();
+    else if (ch == 1) signup_page();
+}
+
+void signup_page(){
+    FILE *fp;
+    fp = fopen("data/credentials/creds.txt", "ab");
+
+    printf("----Sign Up----\n\n");
+    char username[30], password[30];
+    printf("Choose a username: ");
+    scanf("%s", username);
+    fflush(stdin);
+    printf("Choose a password: ");
+    scanf("%s", password);
+
+    fprintf(fp, "%s,%s,", username, password);
+    fclose(fp);
+
+    printf("Successfully created a new account!!");
+}
+
+void login_page(){
+    FILE *fp;
+    fp = fopen("data/credentials/creds.txt", "r");
+
+    printf("----Log In----\n\n");
+    char username[30], password[30], nusername[30], npassword[30];
+    printf("Enter username: ");
+    scanf("%s", username);
+    fflush(stdin);
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    int found = 0;
+
+    while (fscanf(fp, "%29[^,],%29[^,],", nusername, npassword) != EOF) {
+        if (strcmp(nusername, username) == 0 && strcmp(npassword, password) == 0) {
+            printf("\nSuccessfully logged in as %s!!", nusername);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) printf("\nIncorrect username or password!");
+    fclose(fp);
+}
