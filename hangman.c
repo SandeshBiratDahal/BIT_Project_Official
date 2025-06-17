fbeea49ea27a734d07d498a37d90f50fdb17afcf
Sandesh <sandeshdahal063@gmail.com>
2025-06-17 09:27:18 +0545

    Completed the mechanics of the first game, hangman

--- hangman.c
+++ hangman.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+void display(char current_word[], char guessed_word[], int word_length, int number_of_attempts);
+int check_letter(char word[], char letter, int word_length);
+int compare_words(char current_word[], char guessed_word[], int word_length);
 
 void hangman(){
     char words[][100] = {
@@ -16,12 +20,62 @@ void hangman(){
 
     srand(time(0));
     int current_word_index = rand() % number_of_words;
-    char current_word[10], guessed_word[10] = "__________";
+    char current_word[10], guessed_word[10] = "__________", current_letter;
     strcpy(current_word, words[current_word_index]);
 
-    int word_length = strlen(current_word);
+    int word_length = strlen(current_word), number_of_attempts = 5, successfully_guessed = 0, letter_in_word = 0, i;
 
+    while (1){
+        display(current_word, guessed_word, word_length, number_of_attempts);
+        fflush(stdin);
+        printf("Enter a letter: ");
+        scanf("%c", &current_letter);
 
+        letter_in_word = check_letter(current_word, current_letter, word_length);
 
-    printf("---%s---%d---", current_word, word_length);
+        if (!letter_in_word) {
+            printf("Letter not in the word!!");
+            getch();
+            number_of_attempts--;
+        }
+        else {
+            for (i = 0; i < word_length; i++){
+                if (current_word[i] == current_letter) {
+                    guessed_word[i] = current_letter;
+                }
+            }
+            successfully_guessed = compare_words(current_word, guessed_word, word_length);
+        }
+
+        if (!number_of_attempts || successfully_guessed){
+            break;
+        }
+    }
+
+}
+
+void display(char current_word[], char guessed_word[], int word_length, int number_of_attempts){
+    system("cls");
+    printf("----Hangman----\n\n\n");
+
+    int i;
+    
+    for (i = 0; i < word_length; i++) printf("%c  ", guessed_word[i]);
+    printf("\t\tAttempts Left: %d\n\n", number_of_attempts);
+}
+
+int check_letter(char word[], char letter, int word_length){
+    int i;
+    for (i = 0; i < word_length; i++) {
+        if (word[i] == letter) return 1;
+    }
+    return 0;
+}
+
+int compare_words(char current_word[], char guessed_word[], int word_length){
+    int i;
+    for (i = 0; i < word_length; i++){
+        if (current_word[i] != guessed_word[i]) return 0;
+    }
+    return 1;
 }
